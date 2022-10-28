4e4cdc39aa5a145dec5f88c97ebb87d26e6cff4d
skipperbarajin <116503361+skipperbarajin@users.noreply.github.com>
2022-10-28 21:47:09 +0330

    Create convert year to month.cpp

---
+++ convert year to month.cpp
@@ -0,0 +1,13 @@
+//crated by admin in (1401/08/07)_(29/10/2022)
+#include<iostream>
+using namespace std;
+//this program convert year TO month and day
+int main()
+{
+int y;
+cout<<"/n Enter your age:";
+cin>>y;//input
+cout<<"/n month:"<<(y*12);//output
+cout<<"/n day:"<<(y*365);//output
+return 0;
+}
